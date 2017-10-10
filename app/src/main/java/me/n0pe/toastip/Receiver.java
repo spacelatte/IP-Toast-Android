1adcefd4490c17e43e02c6bcc125da4d5671c672
Mert Akengin <mert@n0pe.me>
2017-10-10 21:15:16 +0300

    initial commit
    added initial intellij idea project files
    App.java: Application Entry point (Initializes shake detection)
    Receiver.java: Receives events from OS
    Settings.java: UI for showing to user...

---
+++ app/src/main/java/me/n0pe/toastip/Receiver.java
@@ -0,0 +1,51 @@
+package me.n0pe.toastip;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.wifi.WifiManager;
+import android.preference.PreferenceManager;
+import android.text.format.Formatter;
+import android.util.Log;
+import android.widget.Toast;
+
+public class Receiver extends BroadcastReceiver {
+	String TAG = getClass().getSimpleName();
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d(TAG, "onReceive: " + intent.toUri(Intent.FLAG_DEBUG_LOG_RESOLUTION));
+		
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		String action = intent.getAction();
+		
+		if(!preferences.getBoolean("enabled", true)) {
+			return;
+		}
+		if(action.contains("BOOT") && !preferences.getBoolean("receive_boot", true)) {
+			return;
+		}
+		if(action.contains("WIFI") && !preferences.getBoolean("receive_wifi", true)) {
+			return;
+		}
+		if(action.contains("UNLOCK") && !preferences.getBoolean("receive_unlock", true)) {
+			return;
+		}
+		if(action.contains("SCREEN") && !preferences.getBoolean("receive_screen", true)) {
+			return;
+		}
+		String message = null;
+		try {
+			int ip = ((WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE))
+				.getConnectionInfo().getIpAddress();
+			message = Formatter.formatIpAddress(ip);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		Toast.makeText(
+			context.getApplicationContext(), message,
+			PreferenceManager.getDefaultSharedPreferences(context).getBoolean("length", false) ? 1 : 0
+		).show();
+		return;
+	}
+}

6ac24d805879878f25a6473883be39dda35b2ec0
Mert Akengin <mert@n0pe.me>
2017-12-02 03:06:18 +0200

    bump everything to latest stable

--- app/src/main/java/me/n0pe/toastip/Receiver.java
+++ app/src/main/java/me/n0pe/toastip/Receiver.java
@@ -4,11 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.wifi.WifiManager;
 import android.preference.PreferenceManager;
-import android.text.format.Formatter;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 public class Receiver extends BroadcastReceiver {
 	String TAG = getClass().getSimpleName();
@@ -39,18 +42,31 @@ public class Receiver extends BroadcastReceiver {
 		if(action.contains("SCREEN") && !preferences.getBoolean("receive_screen", true)) {
 			return;
 		}
-		String message = null;
-		try {
-			int ip = ((WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE))
-				.getConnectionInfo().getIpAddress();
-			message = Formatter.formatIpAddress(ip);
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-		}
 		Toast.makeText(
-			context.getApplicationContext(), message,
+			context.getApplicationContext(), getDeviceIPAddress(),
 			PreferenceManager.getDefaultSharedPreferences(context).getBoolean("length", false) ? 1 : 0
 		).show();
 		return;
+	}
+	
+	/// from: https://stackoverflow.com/a/30182851/5601157
+	public static String getDeviceIPAddress() {
+		StringBuilder builder = new StringBuilder();
+		try {
+			List<NetworkInterface> networkInterfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+			for (NetworkInterface networkInterface : networkInterfaces) {
+				List<InetAddress> inetAddresses = Collections.list(networkInterface.getInetAddresses());
+				for (InetAddress inetAddress : inetAddresses) {
+					if (!inetAddress.isLoopbackAddress()) {
+						String addr = inetAddress.getHostAddress();
+						builder.append(addr);
+						builder.append('\n');
+					}
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return builder.toString();
 	}
 }
